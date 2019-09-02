91b6143083df57f868620172d5b80f84095b2f68
SHI2412217299 <2412217299@qq.com>
2019-09-02 15:09:48 +0800

    sty

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import store from './store'
-import '../font/iconfont.css'
+import store from './store'
 import 'lib-flexible/flexible.js'
-import VueResource from 'vue-resource'
+import vueaxios from 'vue-axios'
+import axios from 'axios'
+import 'lib-flexible/flexible.js'
 Vue.config.productionTip = false
-
-Vue.use(VueResource)
+Vue.use(vueaxios,axios)
 
 new Vue({
   router,
-  // store,
+  store,
   render: h => h(App)
 }).$mount('#app')
