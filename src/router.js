2843cfd0899c545c9275032d0fa69fd3f4cf2102
z-yongchao <493492875@qq.com>
2019-09-02 10:20:49 +0800

    .

--- src/router.js
+++ src/router.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
 import hotcity from './views/f-hotcity.vue'
 import fsearch from './views/f-search.vue'
 import Hello from './components/HelloWorld.vue'
-=======
-<<<<<<< HEAD
 import Chotcity from './views/c-hotcity.vue'
 import register from './views/j-register.vue'
-=======
 import Sfoot from './components/Sfoot.vue'
 import Shead from './components/Shead.vue'
 import Search from './components/Search.vue'
@@ -16,9 +12,7 @@ import Mine from './components/Mine.vue'
 import List from './components/List.vue'
 import Zmsite from './views/z-productList.vue'
 import Zfood from './views/z-food.vue'
->>>>>>> a5fce193b1c12b1fd8a885e52cf77220538907d2
 
->>>>>>> 4f78e57c3b21cd21acbe2465eed7b69d3fea27f2
 Vue.use(Router)
 
 export default new Router({
@@ -26,7 +20,6 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-<<<<<<< HEAD
       path: '/',
       name: 'hotcity',
       component: hotcity
@@ -44,7 +37,6 @@ export default new Router({
       name: 'Hello',
       component: Hello
     }
-=======
       path: '/Shead',
       name: 'Shead',
       component: Shead
@@ -76,14 +68,10 @@ export default new Router({
       name: 'Zfood',
       component: Zfood
     },
-<<<<<<< HEAD
     {
       path: '/register',
       name: 'register',
       component: register
     },
-=======
->>>>>>> 4f78e57c3b21cd21acbe2465eed7b69d3fea27f2
->>>>>>> a5fce193b1c12b1fd8a885e52cf77220538907d2
   ]
 })
