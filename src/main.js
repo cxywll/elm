ccfa440a61970dee27fdf1c1abfe08226a293edf
Your Name <you@example.com>
2019-09-02 08:51:39 +0800

    wcj

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+//import store from './store'
 import 'lib-flexible/flexible.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
+axios.defaults.withCredentials = true;
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+//store,
   render: h => h(App)
 }).$mount('#app')
--- src/views/c-hotcity.vue
+++ src/views/c-hotcity.vue
@@ -1,5 +1,10 @@
 <template>
-  <p>22345gdccushxhcgjhcbdcn13</p>
+  <div class="j-box">
+  	<div class="header">
+  		<div class="header-left">&lt;&nbsp;ele.me</div>
+  		<div class="header-right">登录|注册</div>
+  	</div>
+  </div>
 </template>
 
 <script>
@@ -9,6 +14,27 @@ export default {
 </script>
 
 <style>
-
+.header{
+	width: 100%;
+	height: 50px;
+	background: #3190e8;
+}
+.header-left{
+	width: 100px;
+	height: 50px;
+	font-size: 25px;
+	color: white;
+	line-height: 50px;
+	float: left;
+	margin-left: 20px;
+}
+.header-right{
+	width: 120px;
+	height: 50px;
+	color: white;
+	font-size: 25px;  
+	line-height: 50px;
+	float: right;
+}
 </style>
 
