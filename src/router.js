890e1a23ddc47181ebab4094ec1d670be98b4797
zyy-1109 <1491217080@qq.com>
2019-09-03 08:50:02 +0800

    xuanxiang

--- src/router.js
+++ src/router.js
@@ -1,25 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Chotcity from './views/c-hotcity.vue'
+import Yorder from './views/y-order.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'Chotcity',
       component: Chotcity
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
+    {
+      path: '/yorder',
+      name: 'Yorder',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component:Yorder
+    }
   ]
 })
